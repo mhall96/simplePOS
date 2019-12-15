68339d934bf53516bdead4e7c8c89ca75f298864
Matthew Hall <mwhall2900@gmail.com>
2019-12-15 00:01:21 -0600

    and so it begins

---
+++ src/test/java/com/project/simplepos/SimplePosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.simplepos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplePosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
