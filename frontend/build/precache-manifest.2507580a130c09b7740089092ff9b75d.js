8eb77017b1023f6a164e78b98fd57a2c89f76dbf
Victoria Hall <victoria.hall@utexas.edu>
2020-01-18 19:38:29 -0600

    starting login page

---
+++ frontend/build/precache-manifest.2507580a130c09b7740089092ff9b75d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63e5f206b89e91a1e6fb26503b1f272d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a13fca010d7dd0cba90",
+    "url": "/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "07f55cc3cf57a86ac9bb",
+    "url": "/static/js/2.c9f9a8b9.chunk.js"
+  },
+  {
+    "revision": "81ee1eb8974a38b213c38cd7449bcc53",
+    "url": "/static/js/2.c9f9a8b9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3a13fca010d7dd0cba90",
+    "url": "/static/js/main.9357d04e.chunk.js"
+  },
+  {
+    "revision": "05e2d2637e83c42dc136",
+    "url": "/static/js/runtime-main.4ee28378.js"
+  }
+]);
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//import '@appbaseio/reactivesearch/dist/css/style.min.css';
+
 import './index.css';
-import App from './App';
+import Routes from './Routes';
+import LoginPage from "./LoginPage";
+//import Home from "./Home";
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<LoginPage />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
